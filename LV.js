dbc1a3225eec804fbbd414ad4a56337ec2e168c5
Jemmin Chang <jemminchang@cmu.edu>
2014-07-23 11:45:11 -0400

    Endgame tested; working well
    When the interface is up though, should add some code (either on interface
    side by pulling the bestMovesValue as well or on the Game.js side) to mark
    game-ending moves as winning or losing moves.

--- LV.js
+++ LV.js
@@ -28,39 +28,12 @@ function main(data) {
                      't','k','a','i','s',
                      't','m','r','p','r',
                      'y','t','v','o','t'];
-  var realLetters = ['e','k','v','r','t',
-                     'y','z','p','r','i',
-                     'd','z','q','n','g',
-                     'g','k','a','y','m',
-                     'a','b','d','a','m'];
   var testBoard = new Array(25);
-  var realBoard = new Array(25);
-  var realColors = [-1,0,0,-1,1,
-                    0,0,-1,-2,-1,
-                    0,0,0,-1,-2,
-                    -1,0,0,-1,-2,
-                    0,0,0,-1,-2];
-  for (var i = 0; i < 25; i++) {
-    realBoard[i] = [realLetters[i], realColors[i]];
-  }
   for (var i = 0; i < 25; i++) {
     testBoard[i] = [testLetters[i], 0];
   }
-  //var realGame = new Game('real', true, realBoard, ['yammering', 'trinary','parrying'], tst);
   var game = new Game('test', true, testBoard, [], tst);
-//  realGame.play([22, 23, 18, 10, 3, 0, 20, 24, 9, 13, 14, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // daydreaming
-//  realGame.play([2, 23, 3, 18, 9, 13, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // varying
-//  realGame.play([21, 8, 9, 15, 23, 13, 22, 3, 18, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // brigandry
-//  realGame.play([17, 8, 3, 23, 18, 9, 13, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // arraying
-//  realGame.play([22, 0, 2, 23, 13, 20, 15, 17, 3, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // devanagari
-//  realGame.play([23, 8, 3, 17, 9, 15, 13, 0, 10, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // arraigned
-//  realGame.play([7, 23, 15, 17, 13, 9, 6, 0, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // paganizer
-//  realGame.play([22, 0, 7, 3, 23, 2, 9, 13, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // depraving
-//  realGame.play([4, 8, 23, 10, 0, 24, 20, 3, 16, 9, 13, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // trademarking
-//  realGame.play([17, 10, 23, 7, 4, 9, 13, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // readapting
-  //realGame.play([11, 23, 24, 9, 13, 10, 17, 8, 5, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]); // zamindary
   start = new Date();
-  //realGame.findBestMoves(5, 10, 10);
   game.findBestMoves(4, 5, 10);
   end = new Date();
   console.log('findBestMoves took: ' + String(end - start));
